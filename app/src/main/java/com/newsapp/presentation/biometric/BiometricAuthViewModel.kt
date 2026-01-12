2998edecb2b04e2c5dafac93f1bf431cd373742d
wallyson-alves <wallyson.alves@thetrainline.com>
2026-01-12 10:30:53 +0100

    Fix import issue with getvalue

--- app/src/main/java/com/newsapp/presentation/biometric/BiometricAuthViewModel.kt
+++ app/src/main/java/com/newsapp/presentation/biometric/BiometricAuthViewModel.kt
@@ -1,6 +1,8 @@
 package com.newsapp.presentation.biometric
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
